3b2586117f0a44e0ee40e3acf5a645dc41f74667
Shubham Kashyap <shubham.kashyap.jha07@gmail.com>
2026-02-28 16:47:19 +0530

    Initail draft of github - getting started project

---
+++ src/test/java/com/mycompany/repositories/GithubProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
